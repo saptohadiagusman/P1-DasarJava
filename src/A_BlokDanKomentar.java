7b920f730c3014977d16a0b5747f6a4f104f4386
LENOVO <saptoaman86@gmail.com>
2022-03-14 16:47:36 +0700

    Initial Project pertemuan 1 dasar java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,13 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek Dan Java");
+        //Contoh komentar 1 baris
+        /*
+        Contoh Koomentar Lebih Dari Satu Baris
+        */
+
+        /**
+         * Java Doc-->baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
